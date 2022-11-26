52942a994bee66b8d02c3ccc693ada4a7d3c09db
日高 凌 <git@ryohidaka.jp>
2022-11-26 12:51:07 +0900

    v0.2.0 (#5)
    * fix: コメント修正
    
    * fix: レスポンスの変数名を変更
    
    * refactor: 環境変数取得処理を別ファイルに分離
    
    * refactor: 共通リクエスト処理を別ファイルに分離
    
    * fix: ユーザ情報のスコープを修正
    
    * refactor: ユーザ情報取得処理を別ファイルに分離
    
    * docs: パッケージのバージョンを更新

---
+++ src/zaim/dotenv.rs
@@ -0,0 +1,33 @@
+use dotenv::dotenv;
+use std::env;
+
+pub struct Env {
+    pub base_url: String,
+    pub consumer_key: String,
+    pub consumer_secret: String,
+    pub access_token: String,
+    pub token_secret: String,
+    pub oauth_verifier: String,
+}
+
+/// Get Environments
+pub fn get_env() -> Env {
+    dotenv().ok();
+
+    let base_url = env::var("ZAIM_BASE_URL").expect("ZAIM_BASE_URL must be set.");
+    let consumer_key = env::var("ZAIM_CUSTOMER_ID").expect("ZAIM_CUSTOMER_ID must be set.");
+    let consumer_secret =
+        env::var("ZAIM_CUSTOMER_SECRET").expect("ZAIM_CUSTOMER_SECRET must be set.");
+    let access_token = env::var("ZAIM_TOKEN").expect("ZAIM_TOKEN must be set.");
+    let token_secret = env::var("ZAIM_SECRET").expect("ZAIM_SECRET must be set.");
+    let oauth_verifier = env::var("ZAIM_VERIFIER").expect("ZAIM_VERIFIER must be set.");
+
+    Env {
+        base_url,
+        consumer_key,
+        consumer_secret,
+        access_token,
+        token_secret,
+        oauth_verifier,
+    }
+}
---
+++ src/zaim/me.rs
@@ -0,0 +1,36 @@
+use crate::types::me::MeResponse;
+use reqwest::Method;
+
+use super::request::send_request;
+
+/// Fetch Me
+///
+/// Fetch user information during authentication.
+///
+/// # Docs
+/// @see https://dev.zaim.net/home/api#user_verify
+///
+/// # Example
+/// Fetch user information during authentication.
+/// ```
+/// mod types;
+/// use zaim::zaim::me;
+///
+/// #[tokio::main]
+/// async fn main() {
+///   // Fetch Me
+///   let me = me::fetch_me().await.me;
+///   println!("{:?}", me);
+/// }
+/// ```
+pub async fn fetch_me() -> MeResponse {
+    let endpoint = "home/user/verify";
+
+    let res = send_request(endpoint, Method::GET)
+        .await
+        .json::<MeResponse>()
+        .await
+        .expect("failed to convert struct from json");
+
+    res
+}
