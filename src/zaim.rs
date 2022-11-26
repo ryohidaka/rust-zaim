42c0e8d8f1b45d8da59c57f27ed70e10ca3cf948
ryohidaka <git@ryohidaka.jp>
2022-11-26 12:30:49 +0900

    refactor: 環境変数取得処理を別ファイルに分離

--- src/zaim.rs
+++ src/zaim.rs
@@ -1,18 +1,7 @@
-use dotenv::dotenv;
 use reqwest::{Client, Method, Response};
 use reqwest_oauth1::{OAuthClientProvider, Secrets};
-use std::env;
 
 use crate::types::me::MeResponse;
-
-struct Env {
-    base_url: String,
-    consumer_key: String,
-    consumer_secret: String,
-    access_token: String,
-    token_secret: String,
-    oauth_verifier: String,
-}
 
 /// Send Request
 async fn send_request(endpoint: &str, method: Method) -> Response {
@@ -40,27 +29,7 @@ async fn send_request(endpoint: &str, method: Method) -> Response {
     res
 }
 
-/// Get Environments
-fn get_env() -> Env {
-    dotenv().ok();
-
-    let base_url = env::var("ZAIM_BASE_URL").expect("ZAIM_BASE_URL must be set.");
-    let consumer_key = env::var("ZAIM_CUSTOMER_ID").expect("ZAIM_CUSTOMER_ID must be set.");
-    let consumer_secret =
-        env::var("ZAIM_CUSTOMER_SECRET").expect("ZAIM_CUSTOMER_SECRET must be set.");
-    let access_token = env::var("ZAIM_TOKEN").expect("ZAIM_TOKEN must be set.");
-    let token_secret = env::var("ZAIM_SECRET").expect("ZAIM_SECRET must be set.");
-    let oauth_verifier = env::var("ZAIM_VERIFIER").expect("ZAIM_VERIFIER must be set.");
-
-    Env {
-        base_url,
-        consumer_key,
-        consumer_secret,
-        access_token,
-        token_secret,
-        oauth_verifier,
-    }
-}
+pub mod dotenv;
 
 /// Fetch Me
 ///
