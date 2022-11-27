b2c6090519ccbea5fd5f4e4488e52ca8f24c64ba
ryohidaka <git@ryohidaka.jp>
2022-11-27 15:54:09 +0900

    feat: Genre List 取得処理を追加

--- src/types/home.rs
+++ src/types/home.rs
@@ -1,2 +1,3 @@
 pub mod category;
+pub mod genre;
 pub mod me;
--- src/zaim/genre.rs
+++ src/zaim/genre.rs
@@ -1,8 +1,8 @@
 use reqwest::Method;
 
-use crate::types::default::genre::DefaultGenreResponse;
+use crate::types::{default::genre::DefaultGenreResponse, home::genre::GenreResponse};
 
-use super::request::send_request_unauth;
+use super::request::{send_request, send_request_unauth};
 
 /// Fetch Default Genres
 ///
@@ -31,6 +31,39 @@ pub async fn fetch_default_genres() -> DefaultGenreResponse {
     let res = send_request_unauth(endpoint, Method::GET)
         .await
         .json::<DefaultGenreResponse>()
+        .await
+        .expect("failed to convert struct from json");
+
+    res
+}
+
+/// Fetch Genres
+///
+/// Showing the list of your genres.
+///
+/// # Docs
+/// @see https://dev.zaim.net/home/api#genre_home_get
+///
+/// # Example
+/// Showing the list of your genres.
+/// ```
+/// use zaim::zaim::genre;
+///
+/// mod types;
+///
+/// #[tokio::main]
+/// async fn main() {
+///   // Fetch Genres
+///   let genres = genre::fetch_genres().await.genres;
+///   println!("{:?}", genres);
+/// }
+/// ```
+pub async fn fetch_genres() -> GenreResponse {
+    let endpoint = "genre";
+
+    let res = send_request(endpoint, Method::GET)
+        .await
+        .json::<GenreResponse>()
         .await
         .expect("failed to convert struct from json");
 
