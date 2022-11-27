f9edf13ef26f19abb99c6bbeff7573c8692d6683
ryohidaka <git@ryohidaka.jp>
2022-11-27 15:43:03 +0900

    feat: Category List 取得処理を追加

--- src/zaim/category.rs
+++ src/zaim/category.rs
@@ -1,8 +1,8 @@
 use reqwest::Method;
 
-use crate::types::default::category::DefaultCategoryResponse;
+use crate::types::{default::category::DefaultCategoryResponse, home::category::CategoryResponse};
 
-use super::request::send_request_unauth;
+use super::request::{send_request, send_request_unauth};
 
 /// Fetch Default Categories
 ///
@@ -31,6 +31,39 @@ pub async fn fetch_default_categories() -> DefaultCategoryResponse {
     let res = send_request_unauth(endpoint, Method::GET)
         .await
         .json::<DefaultCategoryResponse>()
+        .await
+        .expect("failed to convert struct from json");
+
+    res
+}
+
+/// Fetch Categories
+///
+/// Showing the list of your categories.
+///
+/// # Docs
+/// @see https://dev.zaim.net/home/api#category_home_get
+///
+/// # Example
+/// Showing the list of your categories.
+/// ```
+/// use zaim::zaim::category;
+///
+/// mod types;
+///
+/// #[tokio::main]
+/// async fn main() {
+///   // Fetch Categories
+///   let categories = category::fetch_categories().await.categories;
+///   println!("{:?}", categories);
+/// }
+/// ```
+pub async fn fetch_categories() -> CategoryResponse {
+    let endpoint = "category";
+
+    let res = send_request(endpoint, Method::GET)
+        .await
+        .json::<CategoryResponse>()
         .await
         .expect("failed to convert struct from json");
 
