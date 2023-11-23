f53f789238a694266fd8fec08baabe23e04ecdd7
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-11-23 20:58:46 +0900

    feat: テスト時にAPI_URLを上書きできるように修正

--- src/zaim/dotenv.rs
+++ src/zaim/dotenv.rs
@@ -1,7 +1,10 @@
 use dotenv::dotenv;
 use std::env;
 
+use crate::constants::BASE_URL;
+
 pub struct Env {
+    pub base_url: String,
     pub consumer_key: String,
     pub consumer_secret: String,
     pub access_token: String,
@@ -13,6 +16,7 @@ pub struct Env {
 pub fn get_env() -> Env {
     dotenv().ok();
 
+    let base_url = env::var("BASE_URL").unwrap_or_else(|_| BASE_URL.to_string());
     let consumer_key = env::var("ZAIM_CUSTOMER_ID").expect("ZAIM_CUSTOMER_ID must be set.");
     let consumer_secret =
         env::var("ZAIM_CUSTOMER_SECRET").expect("ZAIM_CUSTOMER_SECRET must be set.");
@@ -21,6 +25,7 @@ pub fn get_env() -> Env {
     let oauth_verifier = env::var("ZAIM_VERIFIER").expect("ZAIM_VERIFIER must be set.");
 
     Env {
+        base_url,
         consumer_key,
         consumer_secret,
         access_token,
